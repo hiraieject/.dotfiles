8746f2c77002f59812e55bed248ec57ed5e5647a
hiraieject <hirai@eject.org>
2019-02-07 09:20:07 +0900

    add initial

---
+++ link.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+export BAKDATE=`date +%y%m%d%H%M`
+
+if [ -f ~/.bashrc ] ; then
+    /bin/rm -f ~/.bashrc.$BAKDATE
+    mv ~/.bashrc ~/.bashrc.$BAKDATE
+fi
+ln -s ~/.dotfiles/.bashrc ~/.bashrc
+
+if [ -f ~/.gdbinit ] ; then
+    /bin/rm -f ~/.gdbinit.$BAKDATE
+    mv ~/.gdbinit ~/.gdbinit.$BAKDATE
+fi
+ln -s ~/.dotfiles/.gdbinit ~/.gdbinit
+
+if [ -d ~/.emacs.d ] ; then
+    /bin/rm -f ~/.emacs.d.$BAKDATE
+    mv ~/.emacs.d ~/.emacs.d.$BAKDATE
+fi
+ln -s ~/.dotfiles/.emacs.d ~/.emacs.d
+
