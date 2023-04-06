9b3413df25f0307f0016b68c63f6541569f42b7a
hiraieject <hiraieject@gmail.com>
2023-04-06 10:24:46 +0900

    .

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -408,16 +408,16 @@
 ;; ;; 何も考えず公式のREADMEからコピペすればいいコード
 ;; ;; straight.el自身のインストールと初期設定を行ってくれる
 
-;; (let ((bootstrap-file (concat user-emacs-directory "straight/repos/straight.el/bootstrap.el"))
-;;       (bootstrap-version 3))
-;;   (unless (file-exists-p bootstrap-file)
-;;     (with-current-buffer
-;;         (url-retrieve-synchronously
-;;          "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-;;          'silent 'inhibit-cookies)
-;;       (goto-char (point-max))
-;;       (eval-print-last-sexp)))
-;;   (load bootstrap-file nil 'nomessage))
+ (let ((bootstrap-file (concat user-emacs-directory "straight/repos/straight.el/bootstrap.el"))
+       (bootstrap-version 3))
+   (unless (file-exists-p bootstrap-file)
+     (with-current-buffer
+         (url-retrieve-synchronously
+          "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+          'silent 'inhibit-cookies)
+       (goto-char (point-max))
+       (eval-print-last-sexp)))
+   (load bootstrap-file nil 'nomessage))
 
 ;; use-package
 (straight-use-package 'use-package)
