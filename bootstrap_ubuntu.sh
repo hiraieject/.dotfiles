36aeb4c0262de0e6ddfcb173d7c25d28fdc6610e
hiraieject <hiraieject@gmail.com>
2024-01-29 09:55:29 +0900

    today's snapshot

---
+++ bootstrap_ubuntu.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt -y install build-essential
+sudo apt -y install git
+sudo apt -y install --reinstall ca-certificates
+sudo apt -y install emacs net-tools global
+sudo apt -y install xterm
+
+if [ ! -f ~/.gitconfig ] ; then
+    git config --global user.email "hiraieject@gmail.com"
+    git config --global user.name "hiraieject"
+fi
+git config --global credential.helper store
+git config pull.rebase false
+git config --global http.sslverify false
+git config --global --add merge.ff false
+git config --global --add pull.ff only
+
+if [ ! -f ~/.git-credentials ] ; then
+    echo 'https://hiraieject@gmail.com:@github.com' > ~/.git-credentials
+    chmod og-rwx ~/.git-credentials
+    echo Please edit ~/.git-credentials
+fi
