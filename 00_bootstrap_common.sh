f9ab8f091a13c354fc2158895e0f12d7a1be4d07
hiraieject <hiraieject@gmail.com>
2024-04-10 17:11:22 +0900

    today's snapshot

--- 00_bootstrap_common.sh
+++ 00_bootstrap_common.sh
@@ -27,3 +27,33 @@ ln -s ~/.dotfiles/Makefile ~/Makefile
 if [ ! -f ~/.dotfiles/.bashrc_local ]; then
     cp ~/.dotfiles/.bashrc_local.template $HOME/.dotfiles/.bashrc_local
 fi
+
+## GIT
+if [ ! -f ~/.gitconfig ] ; then
+    git config --global user.email "hiraieject@gmail.com"
+    git config --global user.name  "hiraieject"
+fi
+git config --global credential.helper store
+git config --global http.sslverify false
+git config --global push.default simple
+git config --global pull.rebase false
+git config --global --add pull.ff only
+git config --global merge.ff false
+
+if [ ! -f ~/.git-credentials ] ; then
+    echo 'https://hiraieject:TOKEN@github.com' > ~/.git-credentials
+    chmod og-rwx ~/.git-credentials
+    echo Please edit ~/.git-credentials
+fi
+
+## OpenAI
+if [ ! -f ~/.openai-credential ] ; then
+    cp .openai-credential-template ~/.openai-credential
+    chmod og-rwx ~/.openai-credential
+fi
+
+## CIFS
+if [ ! -f ~/.cifs-credential ] ; then
+    cp .cifs-credential-template ~/.cifs-credential
+    chmod og-rwx ~/.cifs-credential
+fi
--- 00_bootstrap_ubuntu.sh
+++ 00_bootstrap_ubuntu.sh
@@ -16,24 +16,6 @@ if [ ! -d ~/.ssh ] ; then
     ls -l ~/.ssh/id_rsa*
 fi
 
-## GIT
-if [ ! -f ~/.gitconfig ] ; then
-    git config --global user.email "hiraieject@gmail.com"
-    git config --global user.name  "hiraieject"
-fi
-git config --global credential.helper store
-git config --global http.sslverify false
-git config --global push.default simple
-git config --global pull.rebase false
-git config --global --add pull.ff only
-git config --global merge.ff false
-
-if [ ! -f ~/.git-credentials ] ; then
-    echo 'https://hiraieject:TOKEN@github.com' > ~/.git-credentials
-    chmod og-rwx ~/.git-credentials
-    echo Please edit ~/.git-credentials
-fi
-
 ## NODE.JS
 if [ ! -f ~/.node/bin/node ] ; then
     ./install_nodejs.sh
@@ -43,14 +25,3 @@ fi
 sudo apt install python3-pip
 pip3 install --trusted-host pypi.org --trusted-host pypi.python.org --trusted-host=files.pythonhosted.org openpyxl
 
-## OpenAI
-if [ ! -f ~/.openai-credential ] ; then
-    cp .openai-credential-template ~/.openai-credential
-    chmod og-rwx ~/.openai-credential
-fi
-
-## CIFS
-if [ ! -f ~/.cifs-credential ] ; then
-    cp .cifs-credential-template ~/.cifs-credential
-    chmod og-rwx ~/.cifs-credential
-fi
