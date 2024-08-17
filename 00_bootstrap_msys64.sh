7660668a0eda42487b043c72faa06634cde0a578
hiraieject <hiraieject@gmail.com>
2024-08-17 20:49:48 +0900

    today's snapshot

---
+++ 00_bootstrap_msys64.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+sed -i 's/#MSYS=winsymlinks:nativestrict/MSYS=winsymlinks:nativestrict/g' /c/msys64/msys2.ini
+sed -i 's/#MSYS=winsymlinks:nativestrict/MSYS=winsymlinks:nativestrict/g' /c/msys64/mingw32.ini
+sed -i 's/#MSYS=winsymlinks:nativestrict/MSYS=winsymlinks:nativestrict/g' /c/msys64/mingw64.ini
+sed -i 's/#MSYS=winsymlinks:nativestrict/MSYS=winsymlinks:nativestrict/g' /c/msys64/clang32.ini
+sed -i 's/#MSYS=winsymlinks:nativestrict/MSYS=winsymlinks:nativestrict/g' /c/msys64/clang64.ini
+sed -i 's/#MSYS=winsymlinks:nativestrict/MSYS=winsymlinks:nativestrict/g' /c/msys64/clangarm64.ini
+
