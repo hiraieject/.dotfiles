9e5248c08bc12883eaa8032c22caf49f51a1ea6d
hiraieject <hiraieject@gmail.com>
2024-08-17 15:56:58 +0900

    today's snapshot

---
+++ .emacs.d/init/30_yasnippet.el
@@ -0,0 +1,15 @@
+(when (equal (getenv "EMACSSTRAIGHT") "YES")
+
+  (use-package yasnippet
+    :ensure t
+    :hook ((text-mode
+            prog-mode
+            conf-mode
+            snippet-mode) . yas-minor-mode-on)
+    :init
+    (setq yas-snippet-dir "~/.emacs.d/snippets"))
+
+  (use-package yasnippet-snippets         ; Collection of snippets
+    :ensure t)
+
+  )
